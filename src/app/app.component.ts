97f7823f1be4056ce742916e72d0fb927017a5f9
GPC Admin <gpcadmin@GPCs-Mac-mini.local>
2018-05-17 17:03:59 -0400

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
+import 'rxjs/add/operator/toPromise';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+
+  constructor(private http:HttpClient){}
+  ngOnInit() {
+    console.log("this is the url");
+  }
+
+  onSignUp(username, email, password, confirmation, first_name, last_name, address1, city, phone, zip) {
+    const signUpData = {
+      "identifier": username,
+      "email": email,
+      "password": password,
+      "password_confirmation": confirmation
+    }
+    this.http.post(environment.signUpDatabase, {credentials: signUpData})
+    .toPromise().then(()=>{
+      const Multipassify = require('multipassify');
+      const multipassify = new Multipassify(environment.multipassSecret);
+      const customerData = {email: email, first_name: first_name, last_name: last_name,
+                            multipass_identifier: username, return_to:"https://myshippingtest.myshopify.com/account/",
+                            addresses: [{address1: address1,
+                                        city: city,
+                                        first_name: first_name,
+                                        last_name: last_name,
+                                        phone: phone,
+                                        zip: zip,
+                                        default: true
+                                        }]};
+      const token = multipassify.encode(customerData);
+      const url = multipassify.generateUrl(customerData, "myshippingtest.myshopify.com");
+      window.location.href = url;
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
-
+import { environment } from '../environments/environment';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
